60f11a0ac25821d9a53029e5b26435c5f8b6d32e
Gavin Anderson <anderson.t.gavin@gmail.com>
2025-01-28 22:42:38 -0700

    added transform controller script and script folder

---
+++ AdventureGame/Assets/scripts/TransformController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TransformController : MonoBehaviour
+{
+    private void Update()
+    {
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = Vector3(0, x, 0);
+        transform.position = p;
+
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+
+    }
+}
