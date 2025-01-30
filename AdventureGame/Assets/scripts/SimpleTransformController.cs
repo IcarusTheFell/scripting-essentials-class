a54b85e54c15424d1708a08c4391d38b8b9933be
Gavin Anderson <anderson.t.gavin@gmail.com>
2025-01-29 19:31:22 -0700

    added simpletransformcontroller script
    fixed it

--- AdventureGame/Assets/scripts/SimpleTransformController.cs
+++ AdventureGame/Assets/scripts/SimpleTransformController.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class TransformController : MonoBehaviour
+public class SimpleTransformController : MonoBehaviour
 {
     private void Update()
     {
         var x = Mathf.PingPong(Time.time, 3);
-        var p = Vector3(0, x, 0);
+        var p = new Vector3(0, x, 0);
         transform.position = p;
 
         transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
