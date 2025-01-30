737eb56b6692dac227da581aa917c5919bfe378f
Gavin Anderson <anderson.t.gavin@gmail.com>
2025-01-29 19:44:43 -0700

    changed some values in stc (simple Transform Controller)

--- AdventureGame/Assets/scripts/SimpleTransformController.cs
+++ AdventureGame/Assets/scripts/SimpleTransformController.cs
@@ -4,11 +4,11 @@ public class SimpleTransformController : MonoBehaviour
 {
     private void Update()
     {
-        var x = Mathf.PingPong(Time.time, 3);
+        var x = Mathf.PingPong(Time.time, 4);
         var p = new Vector3(0, x, 0);
         transform.position = p;
 
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 15, 5) * Time.deltaTime);
 
     }
 }
